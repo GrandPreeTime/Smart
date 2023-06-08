21c889d58703cdf090c5debac49edb094538b499
aleksandrmilov <smit97rus@mail.ru>
2023-06-08 08:06:39 +0600

    2 day

---
+++ app/src/main/java/com/example/smart/create_card.java
@@ -0,0 +1,14 @@
+package com.example.smart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_card);
+    }
+}
---
+++ app/src/main/java/com/example/smart/create_parol.java
@@ -0,0 +1,14 @@
+package com.example.smart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_parol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_parol);
+    }
+}
