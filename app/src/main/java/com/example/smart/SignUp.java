e90ef15c8d235fc2d4dc919746188b83334eceff
aleksandrmilov <smit97rus@mail.ru>
2023-06-10 11:52:22 +0600

    5 day

--- app/src/main/java/com/example/smart/SignUp.java
+++ app/src/main/java/com/example/smart/SignUp.java
@@ -2,13 +2,76 @@ package com.example.smart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText eEmail;
+    Button btnNext;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // убираем уведомления
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
+
+        // бинд email и button
+        eEmail = (EditText) findViewById(R.id.email);
+        btnNext = (Button) findViewById(R.id.button);
+
+        // валидация email
+        preferences = getSharedPreferences("UserInfo", 0);
+        btnNext.setOnClickListener((v) -> {
+            String emailValue = eEmail.getText().toString();
+            String emailPattern = "[a-zA-Z0-9._]+@[a-z]+\\.+[a-z]+";
+
+            if ((emailValue.matches(emailPattern))) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("email", emailValue);
+                editor.apply();
+                Toast.makeText(this, "Успешно!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, sms_email.class);
+                startActivity(intent);
+            } else if (!emailValue.matches(emailPattern)) {
+                Toast.makeText(this, "Неверно введен email!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // изменение цвета кнопки
+        eEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (Patterns.EMAIL_ADDRESS.matcher(eEmail.getText().toString()).matches()){
+                    btnNext.setEnabled(true);
+                }
+                else btnNext.setEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/smart/sms_email.java
+++ app/src/main/java/com/example/smart/sms_email.java
@@ -2,13 +2,143 @@ package com.example.smart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class sms_email extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sms_email);
+        EditText first_num, second_num, third_num, fourth_num;
+        int time, CorVal, ResVal;
+        String FCode = "";
+        TextView timer, timeInfo;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sms_email);
+            first_num = findViewById(R.id.firstNum);
+            second_num = findViewById(R.id.secondNum);
+            third_num = findViewById(R.id.thirdNum);
+            fourth_num = findViewById(R.id.fourthNum);
+            timeInfo = findViewById(R.id.infoTimer);
+            CorVal = 1362;
+            time = 59;
+            timer = findViewById(R.id.timer);
+            new CountDownTimer(59000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    timer.setText("" + checkDigit(time));
+                    time--;
+                }
+
+                public void onFinish() {
+                    timer.setText("");
+                    timeInfo.setText("Отправить код заново");
+                }
+            }
+                    .start();
+            //1ый ЭТ с перебросом на 2ой
+            first_num.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (first_num.getText().toString().length() == 1) {
+                        FCode += first_num.getText().toString();
+                        second_num.requestFocus();
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+            //2ый ЭТ с перебросом на 3ий
+            second_num.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (second_num.getText().toString().length() == 1) {
+                        FCode += second_num.getText().toString();
+                        third_num.requestFocus();
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+            //3ий ЭТ с перебросом на 4ый
+            third_num.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (third_num.getText().toString().length() == 1) {
+                        FCode += third_num.getText().toString();
+                        fourth_num.requestFocus();
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+
+            //4ий ЭТ с проверкой
+            fourth_num.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (fourth_num.getText().toString().length() == 1) {
+                        FCode += fourth_num.getText().toString();
+                        ResVal = Integer.parseInt(FCode);
+                    }
+                    if (CorVal != ResVal) {
+                        FCode = "";
+                        Toast errorToast = Toast.makeText(sms_email.this, "Введен неправильный код!", Toast.LENGTH_SHORT);
+                        errorToast.show();
+                    } else if (CorVal == ResVal) {
+                        Intent intent = new Intent(sms_email.this, create_parol.class);
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+        }
+
+        public String checkDigit ( int number){
+            return number <= 9 ? "0" + number : String.valueOf(number);
+        }
+
+        public void onClick (View view){
+            if (CorVal == ResVal) {
+                Intent intent = new Intent(this, create_parol.class);
+                startActivity(intent);
+            } else {
+                Toast errorToast = Toast.makeText(this, "Введен неправильный код!", Toast.LENGTH_SHORT);
+                errorToast.show();
+            }
+        }
     }
-}
